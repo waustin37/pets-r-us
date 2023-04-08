7c87b57d6fd56efce465e237baebe172f7716c75
William John Austin <wiaustin@my365.bellevue.edu>
2023-04-07 22:55:25 -0500

    Adding in Chunks

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express');
 const path = require('path');
 
@@ -32,3 +33,39 @@ app.get('/grooming', (req, res) => {
 app.listen(PORT, () => {
     console.log('Application started and listening on PORT ' + PORT);
 });
+=======
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Pets-R-Us Landing', 
+        message: 'Welcome to Pets-R-Us!'
+    })
+});
+app.get('/index', (req, res) => {
+    res.render('index', {
+        title: 'Pets-R-Us Landing', 
+        message: 'Welcome to Pets-R-Us!'
+    })
+});
+app.get('/grooming', (req, res) => {
+    res.render('grooming', {
+        title: 'Pets-R-Us Grooming',
+        message: 'Grooming Appointments'
+    })
+})
+app.listen(PORT, () => {
+    console.log('Application started and listening on PORT ' + PORT);
+});
+>>>>>>> Stashed changes
