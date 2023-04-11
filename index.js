1689109857b966a2f5d87122e9d1c55acaf50b8f
William John Austin <wiaustin@my365.bellevue.edu>
2023-04-11 17:46:22 -0500

    Adding the Boarding Page

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
 //Here are the Modules we are using
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const pino = require('pino');
 
 //Allows us to use Express 
 const app = express();
@@ -20,6 +22,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
 
 
 //The server to use to find the site
@@ -29,22 +33,39 @@ const PORT = process.env.PORT || 3000;
 //If server receives <petsrus/> or <petsrus/index> they will be routed to the landing page
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Pets-R-Us Landing', 
+        title: 'Pets-R-Us Home', 
         message: 'Welcome to Pets-R-Us!'
     })
 });
 app.get('/index', (req, res) => {
     res.render('index', {
-        title: 'Pets-R-Us Landing', 
+        title: 'Pets-R-Us Home', 
         message: 'Welcome to Pets-R-Us!'
     })
 });
+
+//Routes
 
 //If server receives <petsrus/groomiing> they will be routed to the grooming page
 app.get('/grooming', (req, res) => {
     res.render('grooming', {
         title: 'Pets-R-Us Grooming',
         message: 'Grooming Appointments'
+    })
+})
+
+//If server receives <petsrus/training> they will be routed to the training page
+app.get('/training', (req, res) => {
+    res.render('training', {
+        title: 'Pets-R-Us Training',
+        message: 'Training Appointments'
+    })
+})
+
+app.get('/boarding', (req, res) => {
+    res.render('boarding', {
+        title: 'Pets-R-Us Boarding',
+        message: 'Boarding Appointments'
     })
 })
 
